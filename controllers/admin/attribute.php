555f95cbdd37f3d99a1b85cac5854ec0edbc5497
klmnprst <klmnprst@gmail.com>
2015-10-28 16:47:33 +0300

    added admin3

--- controllers/admin/attribute.php
+++ controllers/admin/attribute.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * TODO: delete attribute
+ */
+
 protect_page();
 print_arr($_GET);
 print_arr($_POST);
@@ -38,6 +42,77 @@ $attribute_group_id = (int)$_GET['add_attributes'];
 
 
 
+/**
+ * Editing attributes
+ */
+if (isset($_POST['edit_attr'])) {
+	$error = false;
+	$attribute_group_id = (int)$_POST['attribute_group_id'];
+	foreach ($_POST['attr'] as $key => $val) {
+		$attribute_id = (int)$key;
+		$name = sanitize($val['name']);
+		$sort_order = (int)$val['sort_order'];
+
+		$query = "UPDATE `attribute` SET `name` = '$name', `sort_order` = '$sort_order' WHERE `attribute_id` = '$attribute_id'";
+		$result = mysqli_query($db,$query);
+			if (!$result) {
+				$error = true;
+			} 
+
+	}
+	if ($error === false) {
+		header("Location: /admin/attribute?edit_attributes=$attribute_group_id");
+		exit();
+	}
+}
+
+
+/**
+ * Editing group attributes
+ */
+if (isset($_POST['edit_group_attr'])) {
+	$attribute_group_id = (int)$_POST['attribute_group_id'];
+	$sort_order = (int)$_POST['sort_order'];
+	$name = sanitize($_POST['name']);
+	$query = "UPDATE `attribute_group` SET `name`='$name', `sort_order`='$sort_order' WHERE `attribute_group_id` = '$attribute_group_id' LIMIT 1";
+	//echo $query;
+	$result = mysqli_query($db,$query);
+	if ($result) {
+		header("Location: /admin/attribute");
+		exit();
+	}
+	
+}
+
+
+
+/**
+ * Delete group attributes
+ */
+if (isset($_GET['del_group_attr'])) {
+	$attribute_group_id = (int)$_GET['del_group_attr'];
+	$query = "SELECT * FROM `attribute` WHERE `attribute_group_id` = '$attribute_group_id'";
+	$result = mysqli_query($db,$query);
+	if (mysqli_num_rows($result)>0) {
+		?>
+		<p><span class="alert label">Внимание, группа аттрибутов не пустая, вначале удалите аттрибуты из группы!</span></p>
+		<?php
+	} else {
+		$query = "DELETE FROM `attribute_group` WHERE `attribute_group_id` = '$attribute_group_id' LIMIT 1";
+		$result = mysqli_query($db,$query);
+		if ($result) {
+			header("Location: /admin/attribute");
+			exit();
+		}
+	}
+}
+/**
+ * Delete group attributes
+ */
+
+
+
+
 
 
 
@@ -48,21 +123,34 @@ $attribute_group_id = (int)$_GET['add_attributes'];
 $query = "SELECT * FROM `attribute_group`";
 $result = mysqli_query($db,$query);
 ?>
+<p><a href="/admin/attributes?add_group_attr" style="color:orangered">Добавить группу аттрибутов</a></p>
 <div class="row" style="padding: 0 15px;">
-	<div class="large-8 columns" style="background: #C4EAF2;">Группа аттрибутов</div>
-	<div class="large-2 columns" style="background: #7EBFD9;">Сортировка</div>
-	<div class="large-2 columns" style="background: #C4EAF2;">Действие</div>
+	<div class="large-8 columns" style="background: palegoldenrod">Группа аттрибутов</div>
+	<div class="large-2 columns" style="background: orange;">Сортировка</div>
+	<div class="large-2 columns" style="background: palegoldenrod">Действие</div>
 </div>
+
 <?php	
 while ($row = mysqli_fetch_assoc($result)) {
-
 ?>
+	
 	<div class="row" style="padding: 0 15px;">
 		<div class="large-8 columns" style="background: #C4EAF2;"><a href="/admin/attribute?edit_group='<?php echo $row['attribute_group_id']; ?>"><?php echo $row['name']; ?></a></div>
 		<div class="large-2 columns" style="background: #7EBFD9;"><?php echo $row['sort_order']; ?></div>
 		<div class="large-2 columns" style="background: #C4EAF2;">
-			<a href="/admin/attribute?edit_attributes=<?php echo $row['attribute_group_id']; ?>"><i class="fi-widget large"></i></a>
-			<a href="/admin/attribute?add_attributes=<?php echo $row['attribute_group_id']; ?>"><i class="fi-plus large"></i></a>
+			<span data-tooltip aria-haspopup="true" class="has-tip" title="Редактировать аттрибуты">
+				<a href="/admin/attribute?edit_attributes=<?php echo $row['attribute_group_id']; ?>"><i class="fi-list-thumbnails large"></i></a>
+			</span>
+			<span data-tooltip aria-haspopup="true" class="has-tip" title="Редактировать группу">
+					<a href="/admin/attribute?edit_group_attr=<?php echo $row['attribute_group_id']; ?>"><i class="fi-wrench large"></i></a>
+			</span>
+			<span data-tooltip aria-haspopup="true" class="has-tip" title="Добавить аттрибуты для группы">
+					<a href="/admin/attribute?add_attributes=<?php echo $row['attribute_group_id']; ?>"><i class="fi-plus large"></i></a>
+			</span>
+			<span data-tooltip aria-haspopup="true" class="has-tip" title="Удалить!">
+					<a href="/admin/attribute?del_group_attr=<?php echo $row['attribute_group_id']; ?>"><i class="fi-x large"></i></a>
+			</span>
+
 		</div>
 	</div>
 <?php
@@ -71,7 +159,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 
 if (isset($_GET['edit_group'])) {
 	$attribute_group_id = (int)$_GET['edit_group'];
-	echo $attribute_group_id;
+	//echo $attribute_group_id;
 }
 
 if (isset($_GET['attribute_name'])) {
@@ -86,6 +174,20 @@ if (isset($_GET['attribute_name'])) {
 		echo 'Error in writing DB!';
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Edit attributes
  */
@@ -93,25 +195,95 @@ if (isset($_GET['edit_attributes'])) {
 	$attribute_group_id = (int)$_GET['edit_attributes'];
 	$query = "SELECT * FROM `attribute` WHERE `attribute_group_id` = '$attribute_group_id'";
 	$result = mysqli_query($db,$query);
-	while ($row = mysqli_fetch_assoc($result)) {
-		echo $row['name']. '<br>';
+	$count = mysqli_num_rows($result);
+	if ($count>0) {
+		echo '<br><form method="post">';
+		while ($row = mysqli_fetch_assoc($result)) { ?>
+			
+			<div class="row">
+				<div class="large-8 columns">
+					<input type="text" name="attr[<?php echo $row['attribute_id']; ?>][name]" value='<?php echo $row['name']; ?>'>
+				</div>
+				<div class="large-2 columns">
+					<input type="text" name="attr[<?php echo $row['attribute_id']; ?>][sort_order]" value='<?php echo $row['sort_order']; ?>'>
+				</div>
+				<div class="large-2 columns">
+					<span data-tooltip aria-haspopup="true" class="has-tip" title="Удалить!">
+						<a href="/admin/attribute?del_attr=<?php echo $row['attribute_id']; ?>"><i class="fi-x large"></i></a>
+					</span>
+				</div>
+			</div>	
+			
+			
+			<?php	
+		}
+		echo '<input type="hidden" name="attribute_group_id" value="' . $attribute_group_id . '">';
+		echo '<input type="submit" name="edit_attr" class="button" value="Изменить">';
+		echo '</form>';
 	}
-	
 }
+/**
+ * Edit attributes
+ */
 
 
 
-?>
 
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 	Editing group attributes
+ */
+if (isset($_GET['edit_group_attr'])) {
+	$attribute_group_id = (int)$_GET['edit_group_attr'];
+	$query = "SELECT * FROM `attribute_group` WHERE `attribute_group_id` = '$attribute_group_id'";
+	$result = mysqli_query($db,$query);
+	if (mysqli_num_rows($result)>0) { 
+		$row = mysqli_fetch_assoc($result);
+		?>
+	<br>
+	<p class="panel">Редактировать группу аттрибутов</p>
+	<form method="post" action="/admin/attribute">
+		<input type="text" name="name" value="<?php echo $row['name']; ?>">
+		<input type="text" name="sort_order" value="<?php echo $row['sort_order']; ?>">
+		<input type="hidden" name="attribute_group_id" value="<?php echo $attribute_group_id; ?>">
+		<input type="submit" name="edit_group_attr"  class="button" value="Изменить">
+	</form>
+
+	<?php }
+
+}
+/**
+ * 	Editing group attributes
+ */
+
+
+
+if (isset($_GET['add_group_attr'])) { ?>
+<br>
 <p class="panel">Добавить группу аттрибутов</p>
 <form>
 <input type="text" name="attribute_name" placeholder="Название группы">
 <input type="text" name="sort_order" placeholder="Порядок сортировки" value="0">
 <input type="submit" name="attr_add" class="button" value="Добавить">
 </form>
-
 <br>
 <?php
+}
+
+
+
+
+
 /**
  * Add atributes
  */
@@ -122,7 +294,8 @@ if (isset($_GET['add_attributes'])) {
 	$row = mysqli_fetch_assoc($result);
 	$name = $row['name'];
  ?>
-<p class="panel">Добавить аттрибуты для группы <?php echo $name; ?></p>
+<br>
+<p class="panel">Добавить аттрибуты для группы <span style="color:orangered"><?php echo $name; ?></span></p>
 <form method="post">
 	<?php for ($i=0; $i < 10; $i++) { ?>
 	<div class="row">
